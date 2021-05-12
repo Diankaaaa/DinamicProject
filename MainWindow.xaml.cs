2647654ebe3c8827fa42273c2cf685a2755662a6
diana <diana@DESKTOP-AE9RS0A>
2021-05-12 22:09:43 +0300

    1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DinamicProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Queue<Student> AddStudent; //Создание очереди
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddStudent = new Queue<Student>();
+        }
+
+        private void ADD_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Получение данных от пользователя
+                string surname = SurnameStudent.Text;
+                float averageScore = float.Parse(AverageScoreStudent.Text);
+
+                //Создание объекта класса Student
+                Student student = new Student(surname, averageScore);
+                AddStudent.Enqueue(student);
+                UpdateQueue();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void UpdateQueue()
+        {
+            OUTPUT.ItemsSource = null;
+            OUTPUT.Items.Clear();
+            OUTPUT.ItemsSource = AddStudent;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinamicProject
+{
+    class Student
+    {
+        string Surname { get; set; }
+        float AverageScore { get; set; }
+
+        public Student(string surname, float averageScore)
+        {
+            Surname = surname;
+            AverageScore = averageScore;
+        }
+    }
+}
